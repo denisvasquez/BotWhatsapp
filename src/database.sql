9ed9c6e1035e1e902a1acd3fcd7e65a55837e0b5
Denis Ismael Vásquez Gómez <gomezdenis150@gmail.com>
2026-04-14 00:04:33 -0600

    ADD: Table Products SQL

--- src/database.sql
+++ src/database.sql
@@ -7,7 +7,16 @@ CREATE TABLE Numbers (
     number_id VARCHAR(50) PRIMARY KEY NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_message TIMESTAMP DEFAULT NULL,
-    active_session BINARY DEFAULT 1,
-    saving_date BINARY DEFAULT 0,
-    asking_product BINARY DEFAULT 0
+    first_message TINYINT(1) DEFAULT 0,
+    active_session TINYINT(1) DEFAULT 1,
+    asking_product TINYINT(1) DEFAULT 0
+);
+
+DROP TABLE IF EXISTS Products;
+
+CREATE TABLE Products (
+    product_id INT (11) PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(150) NOT NULL,
+    product_price DECIMAL(10,2) NOT NULL,
+    product_stock INT(11) NOT NULL
 );
